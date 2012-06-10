789b2bd188d851b5dfe575ded85572ebce045c5a
Rati Wannapanop <rati.wannapanop@gmail.com>
2012-06-10 23:58:24 +0700

    add rule for routing 'dashbaord'

--- config/routes.php
+++ config/routes.php
@@ -2,8 +2,9 @@
 return array(
 	
 	// Petro breadcrumbs routing 
-	'(:segment)/(:num)' => '$1/view/$2',
-	'(:segment)/(:num)/edit' => '$1/edit/$2',
+	'(:segment)/(:num)'        => '$1/view/$2',
+	'(:segment)/(:num)/edit'   => '$1/edit/$2',
 	'(:segment)/(:num)/delete' => '$1/delete/$2',
-	'(:segment)/new' => '$1/create',
+	'(:segment)/new'           => '$1/create',
+	'dashboard/(:segment)'    => 'dashboard/index/$1',	// daashboard
 );
